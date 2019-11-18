1ad1b79a76bfe1848e0893d999bcb273df88ca3d
GyuReeKim <starkim96@naver.com>
2019-11-18 13:54:17 +0900

    login 구조 완성

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import App from './App.vue' // 추가
+import router from './router' // 추가
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
+  router, // 추가
   render: h => h(App)
 }).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,21 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import VueRouter from 'vue-router' // 추가
+import Home from '../views/Home.vue' // 추가
+import Login from '../views/Login.vue' // Login 불러오기
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
+    path: '/', // 어떤 경로를 불러올지
     name: 'home',
-    component: Home
+    component: Home // 어떤 이름으로 불러올지
   },
+  // Login 컴포넌트 등록
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    name: 'login',
+    component: Login
   }
 ]
 
