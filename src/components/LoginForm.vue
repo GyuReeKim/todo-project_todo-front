b7ecb5f52762da7c4994d39c77cf8822f3717e48
GyuReeKim <starkim96@naver.com>
2019-11-19 10:19:27 +0900

    jwt session에 저장

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -30,6 +30,7 @@
 
 <script>
 import axios from 'axios'
+import router from '../router' // index.js가 불러와진다.
 
 export default {
   data: function (){
@@ -49,7 +50,15 @@ export default {
       axios.post('http://localhost:8000/api-token-auth/', this.credential) // object 자체를 넘겨준다.
         .then((res)=>{
           this.loading = true
-          console.log(res)
+          
+          // console.log(res) // res를 출력하면 data의 안에 token 값이 있는 것을 확인할 수 있다.
+          res.data.token
+
+          this.$session.start()
+          this.$session.set('jwt', res.data.token) // 저장을 한다.
+
+          // redirect 기능을 한다.
+          router.push('/') // root 주소(Home.vue)로 밀어준다.
         })
         .catch((error)=>{
           this.loading = true
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue' // 추가
 import router from './router' // 추가
+import VueSession from 'vue-session'
 
 Vue.config.productionTip = false
+
+Vue.use(VueSession) // 개발자도구의 Application 안에있는 Session Storage를 사용할 수 있도록 도와준다.
 
 new Vue({
   router, // 추가
