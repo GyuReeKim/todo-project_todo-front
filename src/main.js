5c2f13522d00148788de2f26e14fffa48eb742b6
GyuReeKim <starkim96@naver.com>
2019-11-20 09:32:12 +0900

    vuex 설치

--- src/main.js
+++ src/main.js
@@ -2,12 +2,16 @@ import Vue from 'vue'
 import App from './App.vue' // 추가
 import router from './router' // 추가
 import VueSession from 'vue-session'
+import store from './store'
 
 Vue.config.productionTip = false
 
 Vue.use(VueSession) // 개발자도구의 Application 안에있는 Session Storage를 사용할 수 있도록 도와준다.
 
 new Vue({
-  router, // 추가
+  // 추가
+  router,
+
+  store,
   render: h => h(App)
 }).$mount('#app')
